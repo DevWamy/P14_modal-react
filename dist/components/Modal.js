126db70bef81d1dcaa3e88d5d90984206bd2e550
Nadia Bolivant <chelseasama@gmail.com>
2023-02-07 15:59:14 +0100

    component modified

---
+++ dist/components/Modal.js
@@ -0,0 +1,33 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+var _cross = _interopRequireDefault(require("./cross.svg"));
+require("../src/modal.css");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const App = props => {
+  if (!props.show) {
+    return null;
+  }
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal ".concat(props.className),
+    onClick: props.onClose
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-content",
+    onClick: e => e.stopPropagation()
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-body"
+  }, props.children, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-close"
+  }, /*#__PURE__*/_react.default.createElement("img", {
+    src: _cross.default,
+    onClick: props.onClose,
+    className: "close",
+    alt: "cross"
+  })))));
+};
+var _default = App;
+exports.default = _default;
